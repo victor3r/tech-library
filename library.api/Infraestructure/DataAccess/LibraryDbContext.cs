f92d04f802c6181d67a6ebc47e15412535044d47
Victor Rabelo <victorabelo1697@gmail.com>
2025-08-27 23:33:01 -0300

    chore: add database file

--- library.api/Infraestructure/DataAccess/LibraryDbContext.cs
+++ library.api/Infraestructure/DataAccess/LibraryDbContext.cs
@@ -1,17 +1,20 @@
 ﻿using library.api.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace library.api.Infraestructure.DataAccess
-{
-    public class LibraryDbContext : DbContext
-    {
-        public DbSet<User> Users { get; set; }
-        public DbSet<Book> Books { get; set; }
-        public DbSet<Checkout> Checkouts { get; set; }
+namespace library.api.Infraestructure.DataAccess;
 
-        override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlite("Data Source=C:\\www\\TechLibraryDb.db");
-        }
+public class LibraryDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Checkout> Checkouts { get; set; }
+
+    override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var currentDirectory = Directory.GetCurrentDirectory();
+        var parentDirectory = Directory.GetParent(currentDirectory)?.FullName;
+        var dbPath = Path.Combine(parentDirectory, "TechLibraryDb.db");
+
+        optionsBuilder.UseSqlite($"Data Source={dbPath}");
     }
 }
